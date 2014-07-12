4cd78e4fde3d0b6f3e6a42e3647df929ae9fde87
Riccardo Pellarin <pellarin.riccardo@gmail.com>
2014-07-11 18:25:25 -0700

    added the GFP protein

--- modeling-scripts_Ski7/exosome.modeling.py
+++ modeling-scripts_Ski7/exosome.modeling.py
@@ -32,24 +32,29 @@ simo = IMP.pmi.representation.Representation(m,upperharmonic=True,disorderedleng
 
 datadirectory="../data/"
 
+
 # compname         hier_name     color    fastafile                      fastaid  pdbname                   chain resrange    read                     beadsize rigid_body super_rigid_body emnum_components, emtxtfilename    emmrcfilename                    
-domains=[("Dis3",  "Dis3_1",     0.0,     datadirectory+"exosome.fasta", "Dis3",  datadirectory+"4IFD.pdb", "J",  (1,237),    None,                    5,       0,         [0],     0,                None,            None),
-         ("Dis3",  "Dis3_2",     0.0,     datadirectory+"exosome.fasta", "Dis3",  datadirectory+"4IFD.pdb", "J",  (238,471),  None,                    5,       0,         [0],     0,                None,            None),
-         ("Dis3",  "Dis3_3",     0.0,     datadirectory+"exosome.fasta", "Dis3",  datadirectory+"4IFD.pdb", "J",  (472,-1),   None,                    5,       0,         [0],     0,                None,            None),
-         ("Rrp45", "Rrp45",      0.1,     datadirectory+"exosome.fasta", "Rrp45", datadirectory+"4IFD.pdb", "A",  (1,-1),     None,                    5,       0,         [0],     0,                None,            None),
-         ("Rrp4",  "Rrp4_1",     0.2,     datadirectory+"exosome.fasta", "Rrp4",  datadirectory+"4IFD.pdb", "H",  (1,102),    None,                    5,       0,         [0],     0,                None,            None),
-         ("Rrp4",  "Rrp4_2",     0.2,     datadirectory+"exosome.fasta", "Rrp4",  datadirectory+"4IFD.pdb", "H",  (103,-1),   None,                    5,       0,         [0],     0,                None,            None),
-         ("Csl4",  "Csl4",       0.3,     datadirectory+"exosome.fasta", "Csl4",  datadirectory+"4IFD.pdb", "I",  (1,-1),     None,                    5,       0,         [0],     0,                None,            None),
-         ("Mtr3",  "Mtr3_1",     0.4,     datadirectory+"exosome.fasta", "Mtr3",  datadirectory+"4IFD.pdb", "F",  (1,30),     None,                    5,       0,         [0],     0,                None,            None),
-         ("Mtr3",  "Mtr3_2",     0.4,     datadirectory+"exosome.fasta", "Mtr3",  datadirectory+"4IFD.pdb", "F",  (31,-1),    None,                    5,       0,         [0],     0,                None,            None),
-         ("Rrp40", "Rrp40_1",    0.5,     datadirectory+"exosome.fasta", "Rrp40", datadirectory+"4IFD.pdb", "G",  (1,60),     None,                    5,       0,         [0],     0,                None,            None),
-         ("Rrp40", "Rrp40_2",    0.5,     datadirectory+"exosome.fasta", "Rrp40", datadirectory+"4IFD.pdb", "G",  (61,-1),    None,                    5,       0,         [0],     0,                None,            None),
-         ("Rrp42", "Rrp42",      0.6,     datadirectory+"exosome.fasta", "Rrp42", datadirectory+"4IFD.pdb", "E",  (1,-1),     None,                    5,       0,         [0],     0,                None,            None),
-         ("Ski6",  "Ski6",       0.7,     datadirectory+"exosome.fasta", "Rrp42", datadirectory+"4IFD.pdb", "B",  (1,-1),     None,                    5,       0,         [0],     0,                None,            None),
-         ("Rrp46", "Rrp46",      0.8,     datadirectory+"exosome.fasta", "Rrp46", datadirectory+"4IFD.pdb", "D",  (1,-1),     None,                    5,       0,         [0],     0,                None,            None),
-         ("Rrp43", "Rrp43",      0.9,     datadirectory+"exosome.fasta", "Rrp43", datadirectory+"4IFD.pdb", "C",  (1,-1),     None,                    5,       0,         [0],     0,                None,            None),
-         ("Lrp1",  "Lrp1",       1.0,     datadirectory+"exosome.fasta", "Lrp1",  None                    , " ",  (1,-1),     None,                    5,       1,         [1],     0,                None,            None),
-         ("Ski7",  "Ski7",       0.65,    datadirectory+"exosome.fasta", "Ski7",  datadirectory+"Ski7_3izq_modeller_vmd.pdb", "X",  (1,-1),    None,   5,       2,         [2],     0,                None,            None)]
+domains=[("Dis3",  "Dis3_1",     0.0,     datadirectory+"exosome.fasta", "Dis3",  datadirectory+"4IFD.pdb", "J",  (1,237,0),    None,                    5,       0,         [0],     0,                None,            None),
+         ("Dis3",  "Dis3_2",     0.0,     datadirectory+"exosome.fasta", "Dis3",  datadirectory+"4IFD.pdb", "J",  (238,471,0),  None,                    5,       0,         [0],     0,                None,            None),
+         ("Dis3",  "Dis3_3",     0.0,     datadirectory+"exosome.fasta", "Dis3",  datadirectory+"4IFD.pdb", "J",  (472,-1,0),   None,                    5,       0,         [0],     0,                None,            None),
+         ("Rrp45", "Rrp45",      0.1,     datadirectory+"exosome.fasta", "Rrp45", datadirectory+"4IFD.pdb", "A",  (1,-1,0),     None,                    5,       0,         [0],     0,                None,            None),
+         ("Rrp4",  "Rrp4_1",     0.2,     datadirectory+"exosome.fasta", "Rrp4",  datadirectory+"4IFD.pdb", "H",  (1,102,0),    None,                    5,       0,         [0],     0,                None,            None),
+         ("Rrp4",  "Rrp4_2",     0.2,     datadirectory+"exosome.fasta", "Rrp4",  datadirectory+"4IFD.pdb", "H",  (103,-1,0),   None,                    5,       0,         [0],     0,                None,            None),
+         ("Csl4",  "Csl4",       0.3,     datadirectory+"exosome.fasta", "Csl4",  datadirectory+"4IFD.pdb", "I",  (1,-1,0),     None,                    5,       0,         [0],     0,                None,            None),
+         ("Mtr3",  "Mtr3_1",     0.4,     datadirectory+"exosome.fasta", "Mtr3",  datadirectory+"4IFD.pdb", "F",  (1,30,0),     None,                    5,       0,         [0],     0,                None,            None),
+         ("Mtr3",  "Mtr3_2",     0.4,     datadirectory+"exosome.fasta", "Mtr3",  datadirectory+"4IFD.pdb", "F",  (31,-1,0),    None,                    5,       0,         [0],     0,                None,            None),
+         ("Rrp40", "Rrp40_1",    0.5,     datadirectory+"exosome.fasta", "Rrp40", datadirectory+"4IFD.pdb", "G",  (1,60,0),     None,                    5,       0,         [0],     0,                None,            None),
+         ("Rrp40", "Rrp40_2",    0.5,     datadirectory+"exosome.fasta", "Rrp40", datadirectory+"4IFD.pdb", "G",  (61,-1,0),    None,                    5,       0,         [0],     0,                None,            None),
+         ("Rrp42", "Rrp42",      0.6,     datadirectory+"exosome.fasta", "Rrp42", datadirectory+"4IFD.pdb", "E",  (1,-1,0),     None,                    5,       0,         [0],     0,                None,            None),
+         ("Ski6",  "Ski6",       0.7,     datadirectory+"exosome.fasta", "Rrp42", datadirectory+"4IFD.pdb", "B",  (1,-1,0),     None,                    5,       0,         [0],     0,                None,            None),
+         ("Rrp46_gfp", "Rrp46_1",  0.8,   datadirectory+"exosome.fasta", "Rrp46_gfp", datadirectory+"4IFD.pdb", "D",  (1,246,0),     None,               5,       0,         [0],     0,                None,            None),
+         ("Rrp46_gfp", "Rrp46_2",  1.0,   datadirectory+"exosome.fasta", "Rrp46_gfp", datadirectory+"GFP_1GFL.pdb", "A",  (1,229,246),     None,         5,       5,         [5],     0,                None,            None),
+         ("Rrp43", "Rrp43",      0.9,     datadirectory+"exosome.fasta", "Rrp43", datadirectory+"4IFD.pdb", "C",  (1,-1,0),     None,                    5,       0,         [0],     0,                None,            None),
+         ("Ski7",  "Ski7",       0.65,    datadirectory+"exosome.fasta", "Ski7",  datadirectory+"Ski7_3izq_modeller_vmd.pdb", "X",  (1,-1),    None,     5,       2,         [2],     0,                None,            None),              
+         ("MPP6",  "MPP6",       0.55,    datadirectory+"exosome.fasta", "Mpp6",  None,                     " ",  (1,-1,0),     None,                    5,       3,         [3],     0,                None,            None)]
+
+
+
 
 bm1=IMP.pmi.macros.BuildModel1(simo)
 bm1.build_model(domains)
@@ -58,15 +63,13 @@ resdensities=bm1.get_density_hierarchies()
 
 # randomize the initial configuration
 
-#simo.shuffle_configuration(100)
+simo.shuffle_configuration(100)
 
 # defines the movers
 
 simo.set_rigid_bodies_max_rot(rbmaxrot)
 simo.set_floppy_bodies_max_trans(fbmaxtrans)
 simo.set_rigid_bodies_max_trans(rbmaxtrans)
-simo.set_floppy_bodies()
-simo.setup_bonds()
 
 outputobjects.append(simo)
 sampleobjects.append(simo)
@@ -80,25 +83,29 @@ outputobjects.append(ev)
 
 
 columnmap={}
-columnmap["Protein1"]="protein1"
-columnmap["Protein2"]="protein2"
-columnmap["Residue1"]="residue1"
-columnmap["Residue2"]="residue2"
-columnmap["IDScore"]="score"
-columnmap["XLUniqueID"]="unique ID"
+columnmap["Protein1"]="Protein 1"
+columnmap["Protein2"]="Protein 2"
+columnmap["Residue1"]="Residue1"
+columnmap["Residue2"]="Residue2"
+columnmap["IDScore"]="Score"
+columnmap["XLUniqueID"]="Unique ID"
+rename={"GFP":"Rrp46_gfp","Rrp46":"Rrp46_gfp"}
+offset={"GFP":246}
 
 ids_map=IMP.pmi.tools.map()
 ids_map.set_map_element(1.0,1.0)
 
 xl1 = IMP.pmi.restraints.crosslinking.ISDCrossLinkMS(simo,
-                                   '../data/exosome_XLMS_column.csv',
+                                   '../data/exosome_XLMS_column07012014.csv',
                                    length=21.0,
                                    slope=0.01,
                                    columnmapping=columnmap,
                                    ids_map=ids_map,
                                    resolution=1.0,
                                    label="DSS",
-                                   csvfile=True)
+                                   csvfile=True,
+                                   rename_dict=rename,
+                                   offset_dict=offset)
 xl1.add_to_model()
 sampleobjects.append(xl1)
 outputobjects.append(xl1)
@@ -108,6 +115,7 @@ psi.set_scale(0.05)
 
 
 # sampling
+
 
 
 simo.optimize_floppy_bodies(100)
