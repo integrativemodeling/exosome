0fc31572412ff9cb7bc6f414a760daf86a624eb3
Ben Webb <ben@salilab.org>
2020-05-07 15:46:01 -0700

    Remove pregenerated analysis outputs

--- test/utils.py
+++ test/utils.py
@@ -1,6 +1,7 @@
 import unittest
 import subprocess
 import shutil
+import os
 
 class TestBase(unittest.TestCase):
     def run_modeling_script(self):
@@ -12,6 +13,9 @@ class TestBase(unittest.TestCase):
 
     def run_analysis_script(self):
         """Run IMP analysis"""
+        # clean pregenerated outputs
+        if os.path.exists('kmeans_weight_500_2'):
+            shutil.rmtree('kmeans_weight_500_2')
         p = subprocess.check_call(["python", 'clustering.py'])
         p = subprocess.check_call(["python", 'precision_rmsf.py', "--test"])
         # todo: assert outputs
